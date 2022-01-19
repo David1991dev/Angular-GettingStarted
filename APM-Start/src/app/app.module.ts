a9cb6529061aaa885a7d0cfa027dfec8d2f58451
HorvathDavid <horvath.david@prekog.hu>
2022-01-19 13:21:57 +0100

    'Lesson_8'

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponeent } from './products/product-list.component';
+
+import { FormsModule } from '@angular/forms';
+import { ConvertTSopacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponeent,
+    ConvertTSopacesPipe,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ APM-Start/src/app/shared/star.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from "@angular/core";
+import { ProductListComponeent } from "../products/product-list.component";
+
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ["./star.component.css"],
+})
+
+
+export class StarComponent implements OnChanges {
+  @Input() rating: number = 0;
+  @Input() productName: string = "null";
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+  @Output() notify: EventEmitter<string> = new EventEmitter<string>();
+  cropWidth: number = 75;
+
+
+
+  ngOnChanges(): void {
+      this.cropWidth = this.rating * 75/5;
+  }
+
+  onClick(): void{
+    this.ratingClicked.emit(`The rating of ${this.productName} is ${this.rating}`);
+    console.log(`The rating of ${this.productName} is ${this.rating}`);
+    this.notify.emit('clicked!')
+
+  }
+}
